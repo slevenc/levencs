f1eaefc50c6583e6c0c2a2a2b3f49fdc7b1360ff
Slevenc <madaode@gmail.com>
2013-01-25 13:34:28 +0800

    添加类扫描器

--- Cms/src/main/java/com/slevenc/ccms/util/factory/ClassFinderFactory.java
+++ Cms/src/main/java/com/slevenc/ccms/util/factory/ClassFinderFactory.java
@@ -1,6 +1,12 @@
 package com.slevenc.ccms.util.factory;
 
+import com.slevenc.ccms.logger.LoggerUtil;
+import com.slevenc.ccms.util.finder.ClassFinder;
 import org.springframework.beans.factory.FactoryBean;
+
+import javax.persistence.Entity;
+import java.lang.annotation.Annotation;
+import java.util.List;
 
 /**
  * 类查找器工厂类
@@ -9,14 +15,34 @@ import org.springframework.beans.factory.FactoryBean;
  * Time: 下午3:43
  */
 public class ClassFinderFactory implements FactoryBean<Class[]> {
-    private Class annotationClass = null;
+    private Class<? extends Annotation> annotationClass = null;
 
-    private String packageToScan = "";
+    private String[] packageToScan;
 
+
+    public void setAnnotationClass(String annotationClass) {
+        try {
+            this.annotationClass = (Class<? extends Annotation>) Class.forName(annotationClass);
+        } catch (Exception ex) {
+        }
+    }
+
+    public void setPackageToScan(String[] packageToScan) {
+        this.packageToScan = packageToScan;
+    }
 
     @Override
     public Class[] getObject() throws Exception {
-        throw new UnsupportedOperationException();
+        Class[] result = new Class[0];
+        if (annotationClass != null && packageToScan != null) {
+            ClassFinder scf = new ClassFinder();
+            scf.setPackageList(packageToScan);
+            List<Class> clazz = scf.findAnnotatedClasses(Entity.class);
+            result = clazz.toArray(new Class[0]);
+        }
+
+        LoggerUtil.i.info("output:" + result);
+        return result;
     }
 
     @Override
